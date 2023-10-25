32ea4e9d0b1983ae5808a8b73134fa387660c23a
Diana Shevtsova <113720492+DianaShe@users.noreply.github.com>
2023-10-25 17:21:29 +0300

    adited forcast format

--- 04-weather-forcast/index.js
+++ 04-weather-forcast/index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const getForcastForInterval = require('./services/weatherAPI');
-const TOKEN = "6930013881:AAGsQ2tlptMz3k7TKvWusM2r9LDBIdVstqI";
+
 const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.onText(/\/start/, (msg) => {
@@ -28,10 +28,10 @@ bot.on('message', async (msg) => {
     const threeHInterval = 'at intervals of 3 hours'
     const sixHInterval = 'at intervals of 6 hours'
     if (msg.text === threeHInterval) {
-        const forcast = await getForcastForInterval(0)
-        bot.sendMessage(msg.chat.id, forcast)
+        const forcast = await getForcastForInterval(3)
+        bot.sendMessage(msg.chat.id, `${forcast}`)
     } else if (msg.text === sixHInterval) {
-        const forcast = await getForcastForInterval(1)
-        bot.sendMessage(msg.chat.id, forcast)
+        const forcast = await getForcastForInterval(6)
+        bot.sendMessage(msg.chat.id, `${forcast}`)
     }
 })
--- 04-weather-forcast/services/weatherAPI.js
+++ 04-weather-forcast/services/weatherAPI.js
@@ -2,8 +2,6 @@ const axios = require("axios");
 
 const BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?units=metric";
 
-const API_KEY = "509c5e82e4b16cc50074073127fe3923";
-
 const LOCATION = {
   lat: 50.51,
   lon: 30.8,
@@ -11,9 +9,9 @@ const LOCATION = {
 
 const URL = `${BASE_URL}&appid=${API_KEY}&lat=${LOCATION.lat}&lon=${LOCATION.lon}`;
 
-const getWeatherForcast = async (value) => {
+const getWeatherForcast = async () => {
   try {
-    const res = await axios.get(`${URL}&cnt=${value}`);
+    const res = await axios.get(`${URL}`);
     return res.data.list;
   } catch (error) {
     console.log(error.message);
@@ -22,24 +20,28 @@ const getWeatherForcast = async (value) => {
 
 const getForcastForInterval = async (value) => {
   try {
-    const int = value === 0 ? 1 : 2;
-    const hours = value === 0 ? 3: 6;
+    const data = await getWeatherForcast();
 
-    const data = await getWeatherForcast(int);
+    const filteredData = data.filter((item, index) => index % 2 === 0);
 
-    const temp = parseInt(data[value].main.temp);
+    const forcast = value === 3 ? data : filteredData;
 
-    const weather = data[value].weather[0].description;
-    const weatherWithFirstLetter = weather.replace(
-      weather[0],
-      weather[0].toUpperCase()
-    );
+    const result = forcast.map((item) => {
+      const temp = parseInt(item.main.temp);
+      const time = new Date(item.dt * 1000).toLocaleString();
+      const weather = item.weather[0].description;
+      const weatherWithFirstLetter = weather.replace(
+        weather[0],
+        weather[0].toUpperCase()
+      );
 
-    const windSpeed = parseInt(data[value].wind.speed);
+      const windSpeed = parseInt(item.wind.speed);
 
-    const humidity = data[value].main.humidity;
+      const humidity = item.main.humidity;
+      return `\n${time}: ${weatherWithFirstLetter}\nTemperature - ${temp} C\nWind - ${windSpeed} m per sec\nHumidity - ${humidity} %`;
+    });
 
-    return `Weather forcast for the next ${hours} hours:\n${weatherWithFirstLetter}\nTemperature - ${temp} C\nWind - ${windSpeed} m per sec\nHumidity - ${humidity} %`;
+    return result;
   } catch (error) {
     console.log(error.message);
   }
